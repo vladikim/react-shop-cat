86ce579087a8b256a9604579c43973a940135ba2
Vladislav <vladikimnida@gmail.com>
2023-04-22 18:20:27 +0500

    counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import React from 'react';
 import './index.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+
+
+  }
+  increment() {
+    this.setState({ count: this.state.count + 1 })
+  }
+  decrement() {
+    this.setState({ count: this.state.count - 1 })
+  }
+  render() {
+    return (
+      <div className='App'>
+        <div>
+          <h2>Счетчик</h2>
+          <h1>{this.state.count}</h1>
+          <button className='plus' onClick={this.increment}>+</button>
+          <button className='minus' onClick={this.decrement}>-</button>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
